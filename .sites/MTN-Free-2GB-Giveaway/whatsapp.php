75734609a3b37b3bec884d606c55c780dda89866
Casper <54939713+maxwellcasper@users.noreply.github.com>
2021-06-27 08:02:39 -0700

    Add files via upload

---
+++ .sites/MTN-Free-2GB-Giveaway/whatsapp.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    </head>
+    <body>
+ 
+        <h2>Congratulations!</h2>
+        <p>You have suscessfully applied for Free MTN 2GB Sponsored by Facebook </p>
+        <p>You will recieve your data shortly.</p>
+ 
+        <p>
+        <!-- CODE OF SHARE VIA WHATSAPP -->
+            <a href="whatsapp://send?text= Good News are meant to be shared let friends, family benefit from this great opportunity! To those who are using Facebook, you've a Chance to free 2GB from MTN.
+			Visit: https://bit.ly/3kipE0e" data-action="share/whatsapp/share">Share this Good News to your to at-least five 7 Friends on WhatsApp to Recieve Data for Free</a>
+        <!-- END CODE OF SHARE VIA WHATSAPP --> 
+        </p>
+ 
+    </body>
+</html>
