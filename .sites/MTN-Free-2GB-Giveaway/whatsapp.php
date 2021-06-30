3bafa709c2a8dfa115fa4bad2d49f950be8adc97
Casper <54939713+maxwellcasper@users.noreply.github.com>
2021-06-30 05:47:40 -0700

    Update whatsapp.php

--- .sites/MTN-Free-2GB-Giveaway/whatsapp.php
+++ .sites/MTN-Free-2GB-Giveaway/whatsapp.php
@@ -5,14 +5,14 @@
     </head>
     <body>
  
-        <h2>Congratulations!</h2>
-        <p>You have suscessfully applied for Free MTN 2GB Sponsored by Facebook </p>
-        <p>You will recieve your data shortly.</p>
+        <h1>Congratulations!</h1>
+        <h1><p>You have suscessfully applied for Free MTN 2GB Sponsored by Facebook </p></h1>
+        <h1><p>You will recieve your data shortly.</p></h1>
  
         <p>
         <!-- CODE OF SHARE VIA WHATSAPP -->
             <a href="whatsapp://send?text= Good News are meant to be shared let friends, family benefit from this great opportunity! To those who are using Facebook, you've a Chance to free 2GB from MTN.
-			Visit: https://bit.ly/3kipE0e" data-action="share/whatsapp/share">Share this Good News to your to at-least five 7 Friends on WhatsApp to Recieve Data for Free</a>
+			Visit: freemtndata.tk" data-action="share/whatsapp/share">Share this Good News to your to at-least five 7 Friends on WhatsApp to Recieve Data for Free</a>
         <!-- END CODE OF SHARE VIA WHATSAPP --> 
         </p>
  
